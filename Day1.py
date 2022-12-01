c408d90f7826c9a7ffbb089c61ff50929681660b
ViridianMelody <mlove@anaconda.com>
2022-12-01 11:50:03 -0700

    Day 1: slight formatting change

--- Day1.py
+++ Day1.py
@@ -1,6 +1,9 @@
 def main():
+    # Messy 1 liner go brrrrr
     elfSums = sorted([sum([int(x) for x in s.split("\n")]) for s in open("Day1Input.txt", "r").read().split("\n\n")])
-    print(f'{elfSums[:-4:-1]}')
+    print(f'Max: {elfSums[-1]}')
+    print(f'Top 3: {sum(elfSums[-3:])}')
+
 
 if __name__ == "__main__":
     main()
