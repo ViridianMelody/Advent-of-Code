860510a5505e1c4b365f5617cb9361f99df8bedb
ViridianMelody <mlove@anaconda.com>
2022-12-01 11:42:43 -0700

    Day 1 part 2

--- Day1.py
+++ Day1.py
@@ -1,5 +1,6 @@
 def main():
-    print(max([sum([int(x) for x in s.split("\n")]) for s in open("Day1Input.txt", "r").read().split("\n\n")]))
+    elfSums = sorted([sum([int(x) for x in s.split("\n")]) for s in open("Day1Input.txt", "r").read().split("\n\n")])
+    print(f'{elfSums[:-4:-1]}')
 
 if __name__ == "__main__":
     main()
